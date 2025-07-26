0b44103d92b9d4a7f8c6b11f93cccd07e524a2ae
ardyutama <ardy.putrautama53@gmail.com>
2025-07-26 16:11:55 +0800

    feat: implement tag filter with more tags

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -127,18 +127,18 @@ export interface PaginationLink {
 
 export interface PaginatedCollection<T> {
 	data: T[];
-    current_page: number;
-    first_page_url: string | null;
-    from: number | null;
-    last_page: number;
-    last_page_url: string | null;
-    links: PaginationLink[];
-    next_page_url: string | null;
-    path: string;
-    per_page: number;
-    prev_page_url: string | null;
-    to: number | null;
-    total: number;
+	current_page: number;
+	first_page_url: string | null;
+	from: number | null;
+	last_page: number;
+	last_page_url: string | null;
+	links: PaginationLink[];
+	next_page_url: string | null;
+	path: string;
+	per_page: number;
+	prev_page_url: string | null;
+	to: number | null;
+	total: number;
 }
 
 export type PageProps<
